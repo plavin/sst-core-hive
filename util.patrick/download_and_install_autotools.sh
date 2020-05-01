ac8dc4c012ebdc0efc13af0b3c118db7323dc37c
Patrick Lavin <plavin3@gatech.edu>
2020-05-01 16:00:29 -0400

    update script for installing autotools

--- util.patrick/download_and_install_autotools.sh
+++ util.patrick/download_and_install_autotools.sh
@@ -1,23 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 #Install location
-myprefix=$HOME/data/local
+PREFIX=$HOME/data/temp/local
+
+# Versions to grab
+M4_VERSION=1.4.18
+AUTOCONF_VERSION=2.69
+AUTOMAKE_VERSION=1.16.2
+LIBTOOL_VERSION=2.4.6
 
 #Get most recent versions
-wget http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz
-wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
-wget http://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz
-wget http://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz
+wget http://ftp.gnu.org/gnu/m4/m4-${M4_VERSION}.tar.gz
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-${AUTOCONF_VERSION}.tar.gz
+wget http://ftp.gnu.org/gnu/automake/automake-${AUTOMAKE_VERSION}.tar.gz
+wget http://ftp.gnu.org/gnu/libtool/libtool-${LIBTOOL_VERSION}.tar.gz
 
 # Decompress
-gzip -dc m4-1.4.18.tar.gz | tar xvf -
-gzip -dc autoconf-2.69.tar.gz | tar xvf -
-gzip -dc automake-1.16.2.tar.gz | tar xvf -
-gzip -dc libtool-2.4.6.tar.gz | tar xvf -
+gzip -dc m4-${M4_VERSION}.tar.gz | tar xvf -
+gzip -dc autoconf-${AUTOCONF_VERSION}.tar.gz | tar xvf -
+gzip -dc automake-${AUTOMAKE_VERSION}.tar.gz | tar xvf -
+gzip -dc libtool-${LIBTOOL_VERSION}.tar.gz | tar xvf -
 
-cd m4-1.4.18
-./configure -C --prefix=$myprefix && make && make install
-cd ../autoconf-2.69
-./configure -C --prefix=$myprefix && make && make install
-cd ../automake-1.16.1
-./configure -C --prefix=$myprefix && make && make install
-cd ../libtool-2.4.6
-./configure -C --prefix=$myprefix && make && make install
+cd m4-${M4_VERSION}
+./configure -C --prefix=$PREFIX && make && make install
+cd ../autoconf-${AUTOCONF_VERSION}
+./configure -C --prefix=$PREFIX && make && make install
+cd ../automake-${AUTOMAKE_VERSION}
+./configure -C --prefix=$PREFIX && make && make install
+cd ../libtool-${LIBTOOL_VERSION}
+./configure -C --prefix=$PREFIX && make && make install
